6f7ce1d0b6804863693856a95ca33350dcdbd137
Ben N <ben@rigeltechnical.com>
2019-08-31 14:59:01 -0500

    Add test for files missing from student starter code

---
+++ student/src/main/java/edu/illinois/cs/cs125/example/Present.java
@@ -0,0 +1,7 @@
+package edu.illinois.cs.cs125.example;
+
+// The student successfully added this file to their submission. It should appear in the overlay.
+
+public class Present {
+    // STUDENT VERSION
+}
