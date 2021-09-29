97a9db1c3fd6f765d432be803d7df93f6b270368
Geoffrey Challen <challen@illinois.edu>
2021-09-29 16:14:25 -0500

    Dependency updates.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,26 @@
 plugins {
-    kotlin("jvm") version "1.4.32"
+    kotlin("jvm") version "1.5.31"
     `java-gradle-plugin`
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "6.1.0"
-    id("org.jmailen.kotlinter") version "3.4.0"
-    id("com.github.ben-manes.versions") version "0.38.0"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("org.jmailen.kotlinter") version "3.6.0"
+    id("com.github.ben-manes.versions") version "0.39.0"
 }
 
 group = "com.github.cs125-illinois"
-version = "2021.3.0"
+version = "2021.9.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
-    implementation(kotlin("reflect"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.31")
     implementation(gradleApi())
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.2")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.2")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.12.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.5")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.5")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.12.5")
 }
 gradlePlugin {
     plugins {
